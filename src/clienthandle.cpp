4753fa2442803faeb30eaf7ca54af3a87786810b
lmaciejonczyk <lmaciejonczyk@atsisa.com>
2017-09-27 13:39:11 +0200

    Keep talking until 'quit' received

--- src/clienthandle.cpp
+++ src/clienthandle.cpp
@@ -6,10 +6,16 @@
 void clienthandle(int socket) {
   char buffer[256];
   const char responseMsq[] = "I've got your message";
+
   memset(buffer, 0, sizeof(buffer));
-
   assert(read(socket, buffer, 255) != -1 && "Error during reading");
-  std::cout << "Here is the client's message: " << buffer << '\n';
 
-  assert(write(socket, responseMsq, sizeof(responseMsq)));
+  while (strcmp(buffer, "quit\n") != 0) {
+    std::cout << "Here is the client's message: " << buffer << '\n';
+
+    assert(write(socket, responseMsq, sizeof(responseMsq)));
+
+    memset(buffer, 0, sizeof(buffer));
+    assert(read(socket, buffer, 255) != -1 && "Error during reading");
+  }
 }
